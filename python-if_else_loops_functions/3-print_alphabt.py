f7a6a33fb671efeaac191eaaad87392c3770599f
hubertmucyo <h.mucyo@alustudent.com>
2023-11-01 02:13:40 -0700

    alpha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
     picker = chr(i)
-    if picker== 'q' or picker== 'e':
+    if picker == 'q' or picker == 'e':
         continue
     print("{:s}".format(picker), end="")
