5892ab0f4c3c04db7b1686a356849447c7e0216c
hubertmucyo <h.mucyo@alustudent.com>
2023-11-02 02:32:04 -0700

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ a = 10
 b = 5
 summ = obj.add(a,b)
 diff = obj.sub(a,b)
-divi = obj.div(a,b)
 mult = obj.mul(a,b)
+divi = obj.div(a,b)
 
-print("{:d} + {:d} = {:d}".format(a,b,summ))
-print("{:d} - {:d} = {:d}".format(a,b,diff))
-print("{:d} / {:d} = {:d}".format(a,b,divi))
-print("{:d} x {:d} = {:d}".format(a,b,mult))
+print("{:d} + {:d} = {:d}".format(a, b, summ))
+print("{:d} - {:d} = {:d}".format(a, b, diff))
+print("{:d} x {:d} = {:d}".format(a, b, mult))
+print("{:d} / {:d} = {:d}".format(a, b, divi))
