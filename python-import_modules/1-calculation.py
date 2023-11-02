c5b57dbca265fe93d8355a556af8b52af9b1ec94
hubertmucyo <h.mucyo@alustudent.com>
2023-11-02 02:34:25 -0700

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1.py as obj
+    import calculator_1 as obj
   
+
 a = 10
 b = 5
 summ = obj.add(a,b)
 diff = obj.sub(a,b)
 mult = obj.mul(a,b)
 divi = obj.div(a,b)
-
 print("{:d} + {:d} = {:d}".format(a, b, summ))
 print("{:d} - {:d} = {:d}".format(a, b, diff))
 print("{:d} x {:d} = {:d}".format(a, b, mult))
