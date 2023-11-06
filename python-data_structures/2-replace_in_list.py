2bf8ce47602c9747108fa3b139d632b0c3914adb
hubertmucyo <h.mucyo@alustudent.com>
2023-11-06 00:55:39 -0800

    replace

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# this prints replaces an element and print both old and new list
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx>= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
