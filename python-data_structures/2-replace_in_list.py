cdc10171a1798e06933ca6c9fffd662f19f41e38
hubertmucyo <h.mucyo@alustudent.com>
2023-11-06 00:58:02 -0800

    replace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 # this prints replaces an element and print both old and new list
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx>= len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
