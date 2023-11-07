8eeae991f3a09e51e88e71978264f8d3b41b5674
hubertmucyo <h.mucyo@alustudent.com>
2023-11-07 01:09:32 -0800

    new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,9 +4,8 @@ def new_in_list(my_list, idx, element):
         copy_lt = my_list[:]
         return copy_lt
     elif idx >= len(my_list):
-        copy_l = my_list[:]
-        return copy_l
+        return copy_lt
     else:
-        copy_two = my_list[:]
-        copy_two[idx] = element
-        return copy_two
+        copy_lt = copy_2
+        copy_2[idx] = element
+        return copy_2
