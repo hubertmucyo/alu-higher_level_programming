2b36ef797c652baf762b06adc93e8649a0794eef
hubertmucyo <h.mucyo@alustudent.com>
2023-11-09 00:28:14 -0800

    indexerror

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
