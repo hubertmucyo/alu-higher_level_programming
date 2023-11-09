e768442d00c654498464e05345510572fd2bed3b
hubertmucyo <h.mucyo@alustudent.com>
2023-11-09 00:40:11 -0800

    print int

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
