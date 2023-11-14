f7bfe1a7093dbd12f09e66f2b8c09d8b56d7dea6
hubertmucyo <h.mucyo@alustudent.com>
2023-11-14 03:59:25 -0800

    complexbyte

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" SOMETHING COOOL """
+import math
+""" SOMETHING COOL """
+
+
+class MagicClass:
+    """ class that create circle area"""
+
+    def __init__(self, radius=0):
+        """ initializing """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """ function defining area """
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """ function defining circuference """
+        return ((2 * math.pi) * self.__radius)
