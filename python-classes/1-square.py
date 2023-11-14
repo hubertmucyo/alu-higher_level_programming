a19e4f09430a10c673b0b78fb69b842ff963c2b2
hubertmucyo <h.mucyo@alustudent.com>
2023-11-14 03:46:09 -0800

    classquare

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+DOCUMENTATION FOR MODULES IN HERE WILL DO THE TRICK
+"""
+
+
+class Square:
+    """
+    CLASS CREATES A PRIVATE SIZE INSTANCE VARIABLE AND ASSIGNS IT
+    """
+    def __init__(self, size):
+        self._Square__size = size
