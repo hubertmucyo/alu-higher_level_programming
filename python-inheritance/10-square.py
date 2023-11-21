17f6dd341b04101fc7e33ccbb48e795dbdbc131b
hubertmucyo <h.mucyo@alustudent.com>
2023-11-21 05:56:30 -0800

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from rectangle
+    """
+    def __init__(self, size):
+        """
+        class instantiation
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
