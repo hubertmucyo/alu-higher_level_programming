f92b1a287a87413c7c0bca112c9440a887ce721e
hubertmucyo <h.mucyo@alustudent.com>
2023-11-21 05:50:22 -0800

    IMPROVE-geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" just a module with one class """
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
