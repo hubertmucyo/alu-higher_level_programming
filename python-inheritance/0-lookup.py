dd6ddd556ed1ec47865b7356d93df18cfe1a91ba
hubertmucyo <h.mucyo@alustudent.com>
2023-11-21 02:44:31 -0800

    dir

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" no module use """
+
+
+def lookup(obj):
+    """ function outputs objects of class """
+    return dir(obj)
--- python-inheritance/push.sh
+++ python-inheritance/push.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 git add .
 git commit -m $1
 git push
