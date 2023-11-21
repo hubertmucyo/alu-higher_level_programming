78454b8926ca8c2e1c48c2357a662d7f542ee690
hubertmucyo <h.mucyo@alustudent.com>
2023-11-21 05:44:37 -0800

    kindclass

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" using no modules """
+
+
+def is_kind_of_class(obj, a_class):
+    """ function returns true for any existence of obj relative """
+    return isinstance(obj, a_class)
