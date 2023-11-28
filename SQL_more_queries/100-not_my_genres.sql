2402bd18fc0b25af4f2b0778ea1c6335c7b21810
hubertmucyo <h.mucyo@alustudent.com>
2023-11-28 05:17:00 -0800

    sql_17

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- not my genre
+SELECT DISTINCT tv_genres.name FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name NOT IN
+(SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title='Dexter')
+ORDER BY tv_genres.name;
