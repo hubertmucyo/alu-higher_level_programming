2e922b219db5fa14dc39dfc258fdbdb1483ae5e2
hubertmucyo <h.mucyo@alustudent.com>
2023-11-28 05:13:15 -0800

    sql_13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- number of shows by genre
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_show_genres
+INNER JOIN tv_genres 
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
