f96825ddf938e86796956107382a47d5060b7049
hubertmucyo <h.mucyo@alustudent.com>
2023-11-29 11:53:16 -0800

    fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
 -- privileges!
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+USERS=("user_0d_1" "user_0d_2")
+
+# Loop through each user and retrieve privileges
+for USER in "${USERS[@]}"; do
+    echo "Privileges for $USER:"
+
+    # Query MySQL server for privileges
+    mysql -u $USER -p -e "SHOW GRANTS;" | grep "GRANT"
+
+    echo "-------------------------------------"
+done
