b6d3092efaec5955f30a1df4e4f1ecb75fc0c63a
hubertmucyo <h.mucyo@alustudent.com>
2023-12-01 08:25:45 -0800

    net10

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-""" api request
-    github"""
-import sys
+"""
+Python script that takes 2 arguments in order to solve this challenge.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    res = requests.get('https://api.github.com/repos/{}/{}/commits'
-                       .format(sys.argv[2], sys.argv[1])).json()
-    count = 0
-    for commit in res:
-        name = commit.get("commit").get("author").get("name")
-        sha = commit.get("sha")
-        print("{}: {}".format(sha, name))
-        count += 1
-        if count == 10:
-            break
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
