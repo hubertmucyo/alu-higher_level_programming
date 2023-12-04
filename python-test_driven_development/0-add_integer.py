146ae87f784995f55a8bc0873e179a0e7ca34fd7
hubertmucyo <h.mucyo@alustudent.com>
2023-12-04 00:47:40 -0800

    lis-moi

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    addition of
+    two integers
+"""
+
+
+def add_integer(a=0, b=98):
+    """ function adds integers """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
