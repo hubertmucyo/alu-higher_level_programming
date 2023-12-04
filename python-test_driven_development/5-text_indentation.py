41ce51d56a2c220ef9c4a32d5964a86c2484b12a
hubertmucyo <h.mucyo@alustudent.com>
2023-12-04 00:56:04 -0800

    indent

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+i#!/usr/bin/python3
+"""
+module for text indentation
+"""
+
+
+def text_indentation(text):
+    """ just the function
+    here """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    line = ""
+    for c in text:
+        line += c
+        if c in ['.', '?', ':']:
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end="")
